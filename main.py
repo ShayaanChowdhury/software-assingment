5a0cfe182de6eaaa679670ab3e4fe85d7e3f1db0
ShayaanChowdhury <163373069+ShayaanChowdhury@users.noreply.github.com>
2024-05-14 19:57:31 +1000

    imoported picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+from PIL import ImageTk, Image
 root =tk.Tk()
 root.title("Area Quiz")
 root.geometry("1000x600") 
@@ -10,18 +10,22 @@ Label1.place(x=325,y=100)
 root.configure(background='deep sky blue')
 
 def button_click():
-    label2=tk.Label(root,)
-    label2.pack()
+    Label2=tk.Label(root,)
+    Label2.pack()
 
 def button_click2():
-    label3=tk.Label(root,)
-    label3.pack()
+    Label3=tk.Label(root,)
+    Label3.pack()
 
 button1=tk.Button(root,text="Formula Sheet", height=3, width=15, bg='gray20', fg='white', relief="raised", command=button_click)
 button1.place(x=455,y=200)
 
 button2=tk.Button(root,text="Start Quiz", height=3, width=15, bg='gray20', fg='white', relief="raised", command=button_click2)
 button2.place(x=455,y=300)
+
+img_1 = ImageTk.PhotoImage(Image.open("img1.png"))
+Label_4= tk.Label( image=img_1)
+Label_4.pack()
 
 
 root.mainloop()
