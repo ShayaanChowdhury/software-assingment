e6d0c82e9a521021e87755d05feabe95062fde14
ShayaanChowdhury <163373069+ShayaanChowdhury@users.noreply.github.com>
2024-05-14 15:08:01 +1000

    Creating homepage
    Added the title and bg

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('im the goat')
+import tkinter as tk
+
+root =tk.Tk()
+root.title("Area Quiz")
+root.geometry("1000x600") 
+
+Label1=tk.Label(root, 
+             text="Learn Area Of Shapes",
+             font=("Times New Roman",30),
+             padx=20,
+             pady=10,
+             borderwidth =2
+             )
+Label1.pack() #showing the label on the screen
+Label1.place(x=325,y=100)
+
+root.mainloop()
