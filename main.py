708fdfc6c0dd21f6591a78e530506af99c67858b
ShayaanChowdhury <163373069+ShayaanChowdhury@users.noreply.github.com>
2024-05-15 22:23:28 +1000

    img2

--- main.py
+++ main.py
@@ -18,16 +18,26 @@ def button_click2():
     Label3.pack()
 
 button1=tk.Button(root,text="Formula Sheet", height=3, width=15, bg='gray20', fg='white', relief="raised", command=button_click)
-button1.place(x=455,y=200)
+button1.place(x=455,y=210)
 
 button2=tk.Button(root,text="Start Quiz", height=3, width=15, bg='gray20', fg='white', relief="raised", command=button_click2)
-button2.place(x=455,y=300)
+button2.place(x=455,y=310)
 
 
 img_1 = ImageTk.PhotoImage(Image.open("img1.png").resize((195,195)))
 Label4= tk.Label(root, image=img_1, bg= 'deep sky blue')
 Label4.place(x=35,y=195)
 
+img_2 = ImageTk.PhotoImage(Image.open("img2.png").resize((195,195)))
+Label5= tk.Label(root, image=img_2, bg= 'deep sky blue')
+Label5.place(x=230,y=195)
 
+img_3 = ImageTk.PhotoImage(Image.open("img3.png").resize((195,195)))
+Label6= tk.Label(root, image=img_3, bg= 'deep sky blue')
+Label6.place(x=615,y=195)
+
+img_4 = ImageTk.PhotoImage(Image.open("img4.png").resize((195,195)))
+Label7= tk.Label(root, image=img_4, bg= 'deep sky blue')
+Label7.place(x=805,y=195)
 
 root.mainloop()
