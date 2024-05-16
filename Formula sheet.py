55042a73a1ee8ae8b16512c394fec7bd6fd7e3e4
ShayaanChowdhury <163373069+ShayaanChowdhury@users.noreply.github.com>
2024-05-16 21:56:53 +1000

    formual sheet
    created formula sheet with all the appropriate pictures of how to calculate area of the shape and changed the colour of back ground

---
+++ Formula sheet.py
@@ -0,0 +1,56 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+
+root =tk.Tk()
+root.title("Area Quiz")
+root.geometry("1000x600") 
+
+Label8=tk.Label(root, text="Formula Sheet", font=("Impact",40), bg='navajo white', padx=20, pady=10, borderwidth =2)
+Label8.pack() 
+Label8.place(x=340,y=20)
+root.configure(background='navajo white')
+
+Label9=tk.Label(root, text="Triangle", font=("Impact",25), bg='navajo white', padx=20, pady=10, borderwidth =2)
+Label9.pack() 
+Label9.place(x=35,y=155)
+root.configure(background='navajo white')
+
+Label10=tk.Label(root, text="Circle", font=("Impact",25), bg='navajo white', padx=20, pady=10, borderwidth =2)
+Label10.pack() 
+Label10.place(x=260,y=155)
+root.configure(background='navajo white')
+
+Label11=tk.Label(root, text="Rectangle", font=("Impact",25), bg='navajo white', padx=20, pady=10, borderwidth =2)
+Label11.pack() 
+Label11.place(x=520,y=155)
+root.configure(background='navajo white')
+
+Label12=tk.Label(root, text="Square", font=("Impact",25), bg='navajo white', padx=20, pady=10, borderwidth =2)
+Label12.pack() 
+Label12.place(x=770,y=155)
+root.configure(background='navajo white')
+
+def button_click():
+    Label13=tk.Label(root,)
+    Label13.pack()
+
+button3=tk.Button(root,text="Back to Home", height=3, width=15, bg='gray20', fg='white', relief="raised", command=button_click)
+button3.place(x=850,y=500)
+
+img_5 = ImageTk.PhotoImage(Image.open("Triangle.png").resize((250,250)))
+Label13= tk.Label(root, image=img_5, bg= 'navajo white')
+Label13.place(x=0,y=220)
+
+img_6 = ImageTk.PhotoImage(Image.open("Circle.png").resize((250,250)))
+Label14= tk.Label(root, image=img_6, bg= 'navajo white')
+Label14.place(x=260,y=210)
+
+img_7 = ImageTk.PhotoImage(Image.open("Rectangle.png").resize((250,250)))
+Label15= tk.Label(root, image=img_7, bg= 'navajo white')
+Label15.place(x=520,y=210)
+
+img_8 = ImageTk.PhotoImage(Image.open("Square.png").resize((230,230)))
+Label16= tk.Label(root, image=img_8, bg= 'navajo white')
+Label16.place(x=770,y=210)
+
+root.mainloop()
--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import tkinter as tk
 from PIL import Image, ImageTk
 root =tk.Tk()
 root.title("Area Quiz")
-root.geometry("1000x600") 
+root.geometry("1000x540") 
 
-Label1=tk.Label(root, text="Learn Area Of Shapes", font=("Times New Roman",30), bg='light sky blue', padx=20, pady=10, borderwidth =2)
+Label1=tk.Label(root, text="Learn Area Of Shapes", font=("Impact",40), bg='navajo white', padx=20, pady=10, borderwidth =2)
 Label1.pack() 
-Label1.place(x=325,y=100)
-root.configure(background='light sky blue')
+Label1.place(x=280,y=80)
+root.configure(background='navajo white')
 
 def button_click():
     Label2=tk.Label(root,)
@@ -25,19 +25,19 @@ button2.place(x=455,y=310)
 
 
 img_1 = ImageTk.PhotoImage(Image.open("img1.png").resize((195,195)))
-Label4= tk.Label(root, image=img_1, bg= 'light sky blue')
-Label4.place(x=35,y=195)
+Label4= tk.Label(root, image=img_1, bg= 'navajo white')
+Label4.place(x=20,y=195)
 
 img_2 = ImageTk.PhotoImage(Image.open("img2.png").resize((195,195)))
-Label5= tk.Label(root, image=img_2, bg= 'light sky blue')
-Label5.place(x=230,y=195)
+Label5= tk.Label(root, image=img_2, bg= 'navajo white')
+Label5.place(x=220,y=195)
 
 img_3 = ImageTk.PhotoImage(Image.open("img3.png").resize((195,195)))
-Label6= tk.Label(root, image=img_3, bg= 'light sky blue')
+Label6= tk.Label(root, image=img_3, bg= 'navajo white')
 Label6.place(x=615,y=195)
 
 img_4 = ImageTk.PhotoImage(Image.open("img4.png").resize((195,195)))
-Label7= tk.Label(root, image=img_4, bg= 'light sky blue')
+Label7= tk.Label(root, image=img_4, bg= 'navajo white')
 Label7.place(x=805,y=195)
 
 root.mainloop()
