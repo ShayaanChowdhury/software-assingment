72e0c9e0c305db8a1de45a36ffa5cc333d348c18
ShayaanChowdhury <163373069+ShayaanChowdhury@users.noreply.github.com>
2024-05-23 19:46:09 +1000

    Q1
    Finished code for Q 1

--- quesiotn1.py
+++ quesiotn1.py
@@ -7,12 +7,25 @@ root.configure(background='navajo white')
 
 Label17 = tk.Label(root, text= "Quiz", font=("Impact", 45), bg='navajo white', padx=20, pady=10, borderwidth=2)
 Label17.pack()
-Label17.place(x=430, y=25)
+Label17.place(x=430, y=15)
 
 button5 = tk.Button(root, text="Next Question", height=3, width=15, bg='gray20', fg='white', relief="raised")
 button5.place(x=850, y=460)
 
 Label18 = tk.Label(root, text = "Question 1: What is the area of a square with side lengths 6?", font=("Comic Sans MS", 17), bg='navajo white',padx=20, pady=10, borderwidth=2)
 Label18.pack()
-Label18.place(x=100, y=155)
+Label18.place(x=50, y=125)
+
+r = tk.IntVar()
+
+option_1 = tk.Radiobutton(root, text= "12", font= ('Comic Sans Ms', 15), value = 1, variable = r, bg= 'navajo white').place(x=60, y=200)
+option_2 = tk.Radiobutton(root, text= "36", font= ('Comic Sans Ms', 15), value = 2, variable = r, bg= 'navajo white').place(x=60, y=250)
+option_3 = tk.Radiobutton(root, text= "24", font= ('Comic Sans Ms', 15), value = 3, variable = r, bg= 'navajo white').place(x=60, y=300)
+option_4 = tk.Radiobutton(root, text= "32", font= ('Comic Sans Ms', 15), value = 4, variable = r, bg= 'navajo white').place(x=60, y=350)
+
+
+
+
+
+
 root.mainloop()
